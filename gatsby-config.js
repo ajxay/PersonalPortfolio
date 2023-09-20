809e941063b36dc913da082d8a9ba606ede169cd
Ajay <mail.ajayasok@gmail.com>
2023-09-21 00:36:51 +0530

    Basic changes and updations done

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Ajay',
     description:
       'Ajay is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://ajayasok.tk', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Ajay',
+        short_name: 'Ajay',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
